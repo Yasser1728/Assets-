949f352d5b935fa957ad5b68c339562ea1019f3b
Tec ( titan-elite-commerce ) <yasserrr.fox17@gmail.com>
2026-02-14 06:51:44 +0200

    Delete piMock.service.js

--- piMock.service.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  broadcast: async ({ amount, wallet }) => {
-    return { fake_payment_id: Math.floor(Math.random()*100000), fake_tx_hash: 'pi_'+Math.random().toString(36).substring(7) };
-  }
-};
